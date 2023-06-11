8fb5176f22252e0495504cc7d8655b3d28c36286
mohammad farhadi <m.sabah.farhadi@gmail.com>
2023-06-11 16:12:29 +0200

    add code sandbox and github links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,6 +13,22 @@ export default function Home() {
             >
                 <div>Transposition</div>
             </Link>
+            <div className="flex justify-between space-x-2 text-xs w-fit">
+                <Link
+                    href="https://codesandbox.io/p/sandbox/encryption-decryption-r25otr"
+                    target="_blank"
+                    className="py-4 px-6 border rounded-lg text-center hover:bg-white hover:shadow-lg transition delay-75"
+                >
+                    <div>Code Sandbox</div>
+                </Link>
+                <Link
+                    href="https://github.com/mamadfar/encryption-decryption__nextjs"
+                    target="_blank"
+                    className="py-4 px-6 border rounded-lg text-center hover:bg-white hover:shadow-lg transition delay-75"
+                >
+                    <div>Github Repo</div>
+                </Link>
+            </div>
         </div>
     )
 }
